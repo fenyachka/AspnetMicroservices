1c6ef856a847e94ea4db24777273fb4eb5ef29fa
Fenya <fenyamuradova@gmail.com>
2021-12-13 07:48:42 +0400

    add Order command, query

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailservice.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailservice
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
